b4b3f7e80f6eb12341b6c3e2ac0fac7b26bd4cf3
Anastasiia Veremiichyk <anastasia.veremi@gmail.com>
2022-09-18 19:02:56 +0200

    Set up project.

---
+++ iExpenseSwiftUI/iExpenseSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseSwiftUIApp.swift
+//  iExpenseSwiftUI
+//
+//  Created by Anastasiia Veremiichyk on 18/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
